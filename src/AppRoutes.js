fe39269454a8e30547bce4707002dfa561db9f1c
unknown <nazar.bylen.ir.2021@lpnu.ua>
2023-07-17 18:42:51 +0300

    simple login and signuppages added

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -3,12 +3,16 @@ import React from "react";
 
 import Home from "./modules/Home/Home"
 import ArtistPage from "./modules/ArtistPage/ArtistPage";
+import Login from "./modules/Login/Login";
+import Signup from "./modules/Signup/Signup";
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/home" element={<Home/>}/>
             <Route path="/artist-page/:id" element={<ArtistPage/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/signup" element={<Signup/>}/>
         </Routes>
     )
 }
--- src/modules/Home/Home.jsx
+++ src/modules/Home/Home.jsx
@@ -3,8 +3,19 @@ import React from "react";
 import "./style.css"
 import ArtistCard from "../../components/ArtistCard/ArtistCard";
 import ArtistCardData from "./ArtistCardData";
+import {useNavigate} from "react-router-dom";
 
 const Home = () => {
+    const navigate=useNavigate();
+
+    const login = () => {
+        navigate("/login")
+    }
+
+    const signup = () => {
+        navigate("/signup")
+    }
+
     return (
         <div className="container-fluid home-root">
             <div className="container">
@@ -16,6 +27,8 @@ const Home = () => {
                         return <ArtistCard key={card.id} id={card.id} name={card.name} img={card.img} link={card.link}/>
                     })}
                 </div>
+                <button onClick={login} className="artis-page-btn">LOGIN</button>
+                <button onClick={signup} className="artis-page-btn">SIGN UP</button>
             </div>
         </div>
     )
