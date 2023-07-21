36eb3cb60bb0b3dc6b57b6536d0d9bf9c9133725
unknown <nazar.bylen.ir.2021@lpnu.ua>
2023-07-21 22:24:30 +0300

    sign up and login added

--- src/modules/Login/Login.jsx
+++ src/modules/Login/Login.jsx
@@ -1,16 +1,35 @@
+import { useForm } from "react-hook-form"
+
 import "./style.css"
+import {http} from '../../http'
 
 const Login = () => {
+    const {
+        register,
+        handleSubmit,
+    } = useForm()
+
+    const onSubmit = async (data) => {
+
+        await http
+            .post('/auth/login', data )
+            .then(response => {
+                localStorage.setItem("access-token",JSON.stringify(response.data.accessToken))
+                localStorage.setItem("refresh-token",JSON.stringify(response.data.refreshToken))
+            })
+            .catch(err => console.log(err))
+    }
+
     return (
         <div className="container-fluid back-login">
             <div className="login">
                 <h2>Login</h2>
-                <form action="http://localhost:5000/login" method="POST">
-                    <label htmlFor="username">Username</label>
-                    <input type="text" id="username" name="username" placeholder="Enter your username"/>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <label htmlFor="email">email</label>
+                    <input type="text" placeholder="Enter your email" {...register("email")}/>
 
                     <label htmlFor="password">Password</label>
-                    <input type="password" id="password" name="password" placeholder="Enter your password" />
+                    <input type="password" placeholder="Enter your password" {...register("password")}/>
 
                     <button className="artis-page-btn" type="submit">Login</button>
                 </form>
--- src/modules/Signup/Signup.jsx
+++ src/modules/Signup/Signup.jsx
@@ -1,16 +1,34 @@
+import { useForm } from "react-hook-form"
+
 import "./style.css"
+import {http} from '../../http'
 
 const Signup = () => {
+    const {
+        register,
+        handleSubmit,
+    } = useForm()
+
+    const onSubmit = async (data) => {
+
+        await http
+            .post('/auth/sign-up', data )
+            .then(response => console.log(response.status, response.data))
+            .catch(err => console.log(err))
+    }
+
+
+
     return (
         <div className="container-fluid back-signup">
             <div className="signup">
                 <h2>Sign Up</h2>
-                <form action="http://localhost:5000/submit" method="POST">
-                    <label htmlFor="username">Username</label>
-                    <input type="text" id="username" name="username" placeholder="Enter your username"/>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <label htmlFor="email">email</label>
+                    <input type="text" placeholder="Enter your email" {...register("email")}/>
 
                     <label htmlFor="password">Password</label>
-                    <input type="password" id="password" name="password" placeholder="Enter your password" />
+                    <input type="password" placeholder="Enter your password" {...register("password")}/>
 
                     <button className="artis-page-btn" type="submit">Submit</button>
                 </form>
